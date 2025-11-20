3e958a584526919baa154c1bee5dd34836429718
= <avula.usedu1@gmail.com>
2025-11-19 19:23:51 -0600

    changed in js

--- simongame.js
+++ simongame.js
@@ -1,3 +1,20 @@
 let body = document.querySelector('body');
 body.style.overflow = 'hidden'
-console.dir(body)
+
+let one = document.querySelector('.one');
+let two = document.querySelector('.two');
+let three = document.querySelector('.three');
+let four = document.querySelector('.four');
+
+let diceGlow = [one, two, three, four]
+
+function randomColorClick() {
+    let randomNumber = Math.floor(Math.random() * 4) + 1;
+    let diceGlowedNowElement = diceGlow[randomNumber - 1]
+    diceGlowedNowElement.style.backgroundColor = 'white'
+
+
+}
+
+    randomColorClick();
+
