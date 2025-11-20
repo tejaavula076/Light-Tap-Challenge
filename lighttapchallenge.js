27762847b946f2ff62b99eb61fb073a0a5297663
= <avula.usedu1@gmail.com>
2025-11-19 20:08:55 -0600

    changes in the style and adding logic

--- lighttapchallenge.js
+++ lighttapchallenge.js
@@ -1,6 +1,11 @@
 let body = document.querySelector('body');
 body.style.overflow = 'hidden'
 
+let score = document.querySelector('.score')
+let highScore = document.getElementsByClassName('highScore')
+score.innerText = 'Score: 0';
+
+
 let one = document.querySelector('.one');
 let two = document.querySelector('.two');
 let three = document.querySelector('.three');
@@ -12,9 +17,17 @@ function randomColorClick() {
     let randomNumber = Math.floor(Math.random() * 4) + 1;
     let diceGlowedNowElement = diceGlow[randomNumber - 1]
     diceGlowedNowElement.style.backgroundColor = 'white'
-
+    circle.innerText = 'Watch'
+    console.log('clicked')
 
 }
 
-randomColorClick();
+let circle = document.querySelector('.circle');
+circle.innerText = 'Play'
+circle.addEventListener('click', () => {
+    randomColorClick()
+}
+)
+
+
 
